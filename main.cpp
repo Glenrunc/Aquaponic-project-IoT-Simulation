b0052d96eab42f992cb0c7ddc573091336ecd2bd
GlenRunc <pourcinematteo@gmail.com>
2023-10-09 16:20:41 +0200

    New

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include <server.hpp>
 
 int main(int argc, char* argv[]){
-
-    Sensor<float> s("Temperature",-60.0,60.0);
-    Sensor<float> s1(s);
-    Sensor<int> s2("Sound",0,200);
-
-    // s.consolWrite();
-    s1.consolWrite();
-    s2.consolWrite();
     
     return 0; 
 }
